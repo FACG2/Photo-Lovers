a230e6f9c5b4e930e0bb8075a048833f727ff9c8
Abdallah <you@example.com>
2017-08-29 15:25:06 +0300

    add images to database

--- database/build.sql
+++ database/build.sql
@@ -5,14 +5,14 @@ DROP TABLE IF EXISTS posts CASCADE;
 CREATE TABLE posts(
   id SERIAL PRIMARY KEY,
   title VARCHAR(100) NOT NULL,
-  link VARCHAR NOT NULL,
-  tags VARCHAR(50) NOT NULL,
+  tags VARCHAR(50) ,
   filePath VARCHAR NOT NULL,
-  likes INT NOT NULL,
+  likes INT NOT NULL
 );
 
-INSERT INTO posts (title, link, tags, filePath, likes) VALUES
-('Steve', 'dd', '#jobs', 'ssd', '2');
-
+INSERT INTO posts (title, tags, filePath, likes) VALUES
+('What a view', '#sea #houses', 'images/sea.jpg', '2'),
+('morning coffe', '#coffe #morning', 'images/coffe.jpg', '5'),
+('my garden', '#window #trees #garden', 'images/window.jpg', '0');
 
 COMMIT;
