62ea18cc8752f961f52f42a9fc2ee5894c380130
Abdallah <you@example.com>
2017-08-30 09:58:07 +0300

    add tests to get queries

--- src/model/queries/get.js
+++ src/model/queries/get.js
@@ -1,5 +1,6 @@
 const dbConnection = require('../database/db_connection.js');
 
+// returns an array with all photos
 const getAllPhotos = (cb) => {
   const sql = {text: 'SELECT id,title,tags,filePath, likes from posts'};
   dbConnection.query(sql, (err, data) => {
@@ -11,10 +12,24 @@ const getAllPhotos = (cb) => {
   });
 };
 
+// returns an object that has
+const getphotoById = (id, cb) => {
+  const sql = {text: 'SELECT id,title,tags,filePath, likes from posts where id = $1',
+    values: [id]};
+  dbConnection.query(sql, (err, data) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, data.rows);
+    }
+  });
+};
+
+// returns an array with photos that includes the passed tag
 const getPhotosByTag = (tag, cb) => {
   const sql = {
-    text: 'SELECT id,title,tags,filePath,likes from posts where tags like %$1%',
-    values: [tag]
+    text: 'SELECT id,title,tags,filePath,likes from posts where tags like $1',
+    values: [`%${tag}%`]
   };
   dbConnection.query(sql, (err, data) => {
     if (err) {
@@ -25,6 +40,7 @@ const getPhotosByTag = (tag, cb) => {
   });
 };
 
+// returns an integer with the number of likes for the given id
 const getLikesByID = (id, cb) => {
   const sql = {
     text: 'SELECT likes from posts where id=$1',
@@ -34,13 +50,14 @@ const getLikesByID = (id, cb) => {
     if (err) {
       cb(err);
     } else {
-      cb(null, data.rows.likes);
+      cb(null, data.rows[0].likes);
     }
   });
 };
 
 module.exports = {
   getAllPhotos,
+  getphotoById,
   getPhotosByTag,
   getLikesByID
 };
--- tests/queries.test.js
+++ tests/queries.test.js
@@ -1,11 +1,55 @@
 const test = require('tape');
-const queries = require('../src/model/queries/get');
+const get = require('../src/model/queries/get');
+const post = require('../src/model/queries/post');
 
 test('get all photos', (t) => {
-  queries.getAllPhotos((err, data) => {
+  get.getAllPhotos((err, data) => {
     t.equal(err, null, 'error should be null');
     t.equal(Array.isArray(data), true, 'should return an array');
     t.equal('filepath' in data[0], true, 'objects should have a filePath key');
+    t.end();
+  });
+});
+
+test('get photo by id', (t) => {
+  var id = 1;
+  get.getphotoById(id, (err, data) => {
+    t.equal(err, null, 'error should be null');
+    t.equal(data.length, 1, 'should retrun an array with only one object');
+    t.equal(data[0].id, id, 'should retrun the same id');
+    t.end();
+  });
+});
+
+test('get photos by tag', (t) => {
+  var tag = 'sea';
+  get.getPhotosByTag(tag, (err, data) => {
+    t.equal(err, null, 'error shoul be null');
+    if (data.length > 0) {
+      t.equal(data[0].tags.includes(tag), true, 'should return a photo with the given tag');
+    }
+    t.end();
+  });
+});
+
+test('get number of likes by id', (t) => {
+  var id = 1;
+  get.getLikesByID(id, (err, data) => {
+    t.equal(err, null, 'error should be null');
+    t.equal(Number.isInteger(data), true, 'should return the number of likes');
+    t.end();
+  });
+});
+
+test('insert a photo', (t) => {
+  var photo = {title: 'Tasty!',
+    tags: '#watermelon #plate #tasty',
+    filePath: '/images/watermelon.jpg'
+  };
+  post.addPhoto(photo, (err, data) => {
+    t.equal(err, null, 'error should be null');
+    t.equal(Number.isInteger(data.id), true, 'should return a number as an Id');
+    t.equal(data.title, photo.title, `${data.title} shoul equald ${photo.title}`);
     t.end();
   });
 });
