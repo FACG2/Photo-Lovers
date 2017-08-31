184157381ebf96024e0d5eedd4361487032e9d3f
Abdallah <you@example.com>
2017-08-31 09:05:25 +0300

    delete node modules relates #23

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,12 +1,10 @@
 const pic = require('../model/index.js');
 
 exports.get = (req, res, next) => {
-  console.log('sdfbn');
   pic.get.getAllPhotos((err, data) => {
     if (err) {
       next(err);
     } else {
-      console.log(data);
       res.render('home', {data});
     }
   });
